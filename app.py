451faf7aa5a9999e2ac39f61d0e039f25b66261f
germanter <usersuer32@gmail.com>
2024-12-19 22:20:42 +0400

    added html

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
+data =[
+    {'title':'Commander'},
+    {'title':'Weaponist'},
+    {'title':'Buster'}
+]
 @app.route("/")
 def home():
-    return 'Hello from D.E.A'
+    return render_template('home.html',data=data)
 
 if __name__=="__main__":
     app.run(
