e248469ff0d81c9bad24e7a31c75dbb38de51004
germanter <usersuer32@gmail.com>
2024-12-22 00:19:49 +0400

    api/data added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,jsonify
-from database import load_data
+from database import load_data,load_job
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,6 +10,16 @@ def home():
 @app.route("/api/data")
 def api():
     return jsonify(load_data())
+
+@app.route("/api/data/<id>")
+def job(id):
+    job = load_job(id)
+    if not job == []:
+        return jsonify(job)
+    else:
+        return jsonify('invalid id')
+
+        
 
 if __name__=="__main__":
     app.run(
--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 import pymysql
-# from dotenv import load_dotenv
+# from dotenv import load_dotenv  ###shut down all dotenv in push
 import os
 
-# load_dotenv()  ###security
+# load_dotenv()  ###shut down all dotenv in push
 def load_data():
     timeout = 10
     connection = pymysql.connect(
@@ -22,6 +22,35 @@ def load_data():
         dataset = []
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM jobs")
+        info = cursor.fetchall()
+        for i in info:
+            dataset.append(dict(i))
+        print("SUCCESSFUL DATABASE CONNECTION!")
+        return dataset
+    except:
+        print('FAILED DATABASE CONNECTION!')
+    finally:
+        connection.close()
+        
+def load_job(id):
+    timeout = 10
+    connection = pymysql.connect(
+    charset="utf8mb4",
+    connect_timeout=timeout,
+    cursorclass=pymysql.cursors.DictCursor,
+    db="defaultdb",
+    host="mysql-2b98ccbd-usersuer32-ae57.e.aivencloud.com",
+    password=os.getenv('passdb'),  ###security
+    read_timeout=timeout,
+    port=10406,
+    user="avnadmin",
+    write_timeout=timeout,
+    )
+    
+    try:
+        dataset = []
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM jobs WHERE id = %s",(id,))
         info = cursor.fetchall()
         for i in info:
             dataset.append(dict(i))
